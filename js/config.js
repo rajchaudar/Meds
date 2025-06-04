e48a75fe48721ab7192de03ad9a05a27731cc810
rajchaudar <rajchaudar60@gmail.com>
2025-06-04 14:43:52 +0530

    first commit

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 const API_BASE_URL = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
     ? "http://localhost:3000/api"
-    : "https://meds.up.railway.app/api";
+    : "https://meds.shivrajchaudar.me/api";
